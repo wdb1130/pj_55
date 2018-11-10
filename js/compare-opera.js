740e8d819e59927be563f328e46ba954792c11c3
shiweiwei <shiweiwei@netposa.com>
2018-11-10 20:35:54 +0800

    add对比操作页面

---
+++ js/compare-opera.js
@@ -0,0 +1,8 @@
+$(function () {
+    layui.use('form', function () {
+        var form = layui.form;
+        form.on('select(reportType)', function (data) {
+            console.log(data.value)
+        });
+    });
+});
