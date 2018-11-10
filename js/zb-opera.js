af8170386ce7543bd43c5741a6305c479f5917c1
shiweiwei <shiweiwei@netposa.com>
2018-11-10 16:11:33 +0800

    edit

--- js/zb-opera.js
+++ js/zb-opera.js
@@ -25,25 +25,38 @@ $(function () {
     layui.use('table', function () {
         var table = layui.table;
         table.render({
-            id: 'id',
+            id: 'reportTable',
             elem: '#reportTable',
             url: '../test-json/reportTable.json',
+            // page: true, 
             cols: [
                 [
-                    { type: 'checkbox' }, 
-                    { field: 'id', width: 80, title: 'ID', align: 'center'}, 
-                    { field: 'title', width: 150, title: '名称', align: 'center' }, 
-                    { field: 'cve', width: 150, title: 'CVE', align: 'center' }, 
-                    { field: 'cvss', width: 80, title: 'CVSS', align: 'center' }, 
-                    { field: 'risk', width: 150, title: 'risk', align: 'center' }, 
-                    { field: 'num', width: 100, title: '主机数量', align: 'center' }, 
+                    { type: 'checkbox' },
+                    // { field: 'id', width: 80, title: 'ID', align: 'center' },
+                    { field: 'title', width: 150, title: '名称', align: 'center' },
+                    { field: 'cve', width: 150, title: 'CVE', align: 'center' },
+                    { field: 'cvss', width: 80, title: 'CVSS', align: 'center' },
+                    { field: 'risk', width: 150, title: 'risk', align: 'center' },
+                    { field: 'num', width: 100, title: '主机数量', align: 'center' },
                     { field: 'intro', title: '简介', align: 'center' }
                 ]
-            ], 
-            // page: true, 
-            done: function(res,curr,count){
-                $("[data-field='id']").css('display','none');
+            ],
+            done: function (res, curr, count) {
+                $("[data-field='id']").css('display', 'none');
             }
         });
+        var $ = layui.$, active = {
+            getCheckData: function () { //获取选中数据
+                var checkStatus = table.checkStatus('reportTable');
+                var data = checkStatus.data;
+                console.log(data);
+                layer.alert(JSON.stringify(data));
+            }
+        };
+        $('.download').on('click', function () {
+            var type = $(this).data('type');
+            active[type] ? active[type].call(this) : '';
+        });
     });
+
 });
