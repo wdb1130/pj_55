38daea2a520c02f879733e9681e2c3492fd80e0c
shiweiwei <shiweiwei@netposa.com>
2018-11-12 17:51:01 +0800

    edit模态框例子

--- example/例子-模态框/initChart.js
+++ example/例子-模态框/initChart.js
@@ -1,4 +1,4 @@
-function initChart(chartDom) {
+function initChart(chartDom, legend, xAxis, series) {
     var dom = document.getElementById(chartDom);
     var myChart = echarts.init(dom);
     option = null;
@@ -16,7 +16,7 @@ function initChart(chartDom) {
             }
         },
         legend: {
-            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+            data: legend
         },
         toolbox: {
             feature: {
@@ -33,7 +33,7 @@ function initChart(chartDom) {
             {
                 type: 'category',
                 boundaryGap: false,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                data: xAxis
             }
         ],
         yAxis: [
@@ -41,51 +41,10 @@ function initChart(chartDom) {
                 type: 'value'
             }
         ],
-        series: [
-            {
-                name: '邮件营销',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-                name: '联盟广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-                name: '视频广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {},
-                data: [150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name: '直接访问',
-                type: 'line',
-                stack: '总量',
-                areaStyle: { normal: {} },
-                data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name: '搜索引擎',
-                type: 'line',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'top'
-                    }
-                },
-                areaStyle: { normal: {} },
-                data: [820, 932, 901, 934, 1290, 1330, 1320]
-            }
-        ]
+        series: series
     };
     if (option && typeof option === "object") {
         myChart.setOption(option, true);
+        window.onresize = myChart.resize;
     }
 }
