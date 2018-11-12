ee116d608a0fac40243fc545d6e1c7900b69d81f
wdb <513052904@qq.com>
2018-11-13 00:16:13 +0800

    网络态势修改

--- js/networkData.js
+++ js/networkData.js
@@ -4,6 +4,7 @@ $(function() {
     initChart2();
     initChart3();
     drawChart5();
+    drawChart6();
 })
 
 function initChart1() {
@@ -121,31 +122,43 @@ function initChart2() {
     var dom = document.getElementById("chart-2");
     var myChart2 = echarts.init(dom);
     var option = {
-        grid: {
-            left: '10%',
-            right: '10%',
-            top: '10%'
+        title: {
+            text: '保密系统更新率',
+            bottom: '8%',
+            left:'25%',
+            textStyle: {
+                fontSize: 14,
+                color: '#fff',
+                align: 'center',
+                verticalAlign:'bottom',
+            }
         },
         series: [{
             type: 'liquidFill',
-            radius: '60%',
+            radius: '85%',
             data: [0.5, 0.45, 0.4, 0.3],
             // 水球颜色
             color: ['#FE5555', '#F07581', '#FB5E61'],
-            center: ['40%', '40%'],
+            center: ['57%', '40%'],
             outline: {
                 // show: false
-                // borderDistance: 5,
+                borderDistance: 5,
                 itemStyle: {
-                    borderWidth: 5,
+                    borderWidth: 2,
                     borderColor: '#13FDCE',
                 },
             },
+            backgroundStyle: { //背景颜色设置
+                // color: new echarts.graphic.RadialGradient(['center','red', 'blue']),
+                opacity: 0.4
+            },
+                    // "#160f64",
             label: {
                 normal: {
-                    color: 'red',
-                    insideColor: 'yellow',
-                    fontSize: 50
+                    color: '#fff',
+                    insideColor: '#ccc',
+                    fontSize: 25,
+                    position :['50%','30%']
                 }
             }
         }]
@@ -160,16 +173,55 @@ function initChart3() {
     var dom = document.getElementById("chart-3");
     var myChart3 = echarts.init(dom);
     var option = {
+        title: {
+            text: '文件系统更新率',
+            bottom: '8%',
+            left:'25%',
+            textStyle: {
+                fontSize: 14,
+                color: '#fff',
+                align: 'center',
+                verticalAlign:'bottom',
+            }
+        },
         series: [{
             type: 'liquidFill',
-            radius: '60%',
-            center: ['40%', '40%'],
-            data: [0.5, 0.45, 0.4, 0.3],
-            label: {
+            name: 'hello chiana',
+            radius: '85%',
+            center: ['57%', '40%'],
+            color: ['#661fca','#4a19a1'],
+            data: [0.45,0.44],
+            outline: { //外边框样式
+                show: true,
+                borderDistance: 5,
+                itemStyle: {
+                    color: 'none',
+                    borderColor: '#203487',
+                    borderWidth: 2,
+                    shadowBlur: 20,
+                    shadowColor: 'rgba(0, 0, 0, 0.25)'
+                }
+            },
+            backgroundStyle: { //背景颜色设置
+                color:"#160f64",
+                opacity: 0.4
+            },
+            // itemStyle: {
+            //     opacity: 0.95,
+            //     shadowBlur: 50,
+            //     shadowColor: 'rgba(0, 0, 0, 1)'
+            // },
+            // emphasis: {
+            //     itemStyle: {
+            //         opacity: 0.8
+            //     }
+            // },
+            label: { //标签样式
                 normal: {
-                    color: 'red',
-                    insideColor: 'yellow',
-                    fontSize: 40
+                    color: '#fff',
+                    insideColor: '#ccc',
+                    fontSize: 25,
+                    position :['50%','30%']
                 }
             }
         }]
@@ -179,7 +231,7 @@ function initChart3() {
         window.onresize = myChart3.resize;
     }
 }
-// chart4
+// chart5
 function drawChart5() {
     var dom = document.getElementById("chart-5");
     var myChart5 = echarts.init(dom);
@@ -192,7 +244,6 @@ function drawChart5() {
         name: '未交卷时间'
     }];
     var innerColor = '#2bff8f';
-    var outColor = "#50e0ff";
     var textColor = '#50e0ff';
     var startColor = 'rgba(73,223,240,0.1)';
     var endColor = 'rgba(73,223,240,0.8)';
@@ -249,8 +300,8 @@ function drawChart5() {
                 type: 'pie',
                 clockWise: false,
                 hoverAnimation: false,
-                center: ['20%', '40%'],
-                radius: ['65%', '67.5%'],
+                center: ['50%', '40%'],
+                radius: ['75%', '67.5%'],
                 itemStyle: {
                     normal: {
                         color: innerColor
@@ -281,8 +332,8 @@ function drawChart5() {
                 type: 'pie',
                 clockWise: false,
                 hoverAnimation: false,
-                center: ['20%', '40%'],
-                radius: ['65%', '55%'],
+                center: ['50%', '40%'],
+                radius: ['67.5%', '55%'],
                 color: color,
                 itemStyle: {
                     normal: {
@@ -301,5 +352,128 @@ function drawChart5() {
     if (option && typeof option === "object") {
         myChart5.setOption(option, true);
         window.onresize = myChart5.resize;
+    }
+}
+// chart6
+function drawChart6() {
+    var dom = document.getElementById("chart-6");
+    var myChart6 = echarts.init(dom);
+    option = null;
+    var echartData = [{
+        value: 50,
+        name: '交卷时间'
+    }, {
+        value: 50,
+        name: '未交卷时间'
+    }];
+    var innerColor = '#2bff8f';
+    var textColor = '#50e0ff';
+    var startColor = 'rgba(73,223,240,0.1)';
+    var endColor = 'rgba(73,223,240,0.8)';
+    var scale = 1;
+    var color = [{
+        type: 'linear',
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        colorStops: [{
+            offset: 0,
+            color: startColor
+        }, {
+            offset: 1,
+            color: endColor
+        }],
+        globalCoord: false
+    }, 'none'];
+    var rich = {
+        time: {
+            color: innerColor,
+            fontSize: 32 * scale,
+            padding: [0, 0],
+            fontWeight: 'bold'
+        },
+        unit: {
+            color: innerColor,
+            fontSize: 14 * scale,
+            padding: [0, 0, 0, 0],
+            verticalAlign: 'bottom',
+        }
+    }
+    option = {
+        title: [{
+            text: '网络宽带占用率',
+            x: '50%',
+            y: '80%',
+            textAlign: 'center',
+            textStyle: {
+                color: '#fff',
+                textAlign: 'center',
+                fontSize: 15 * scale,
+                fontWeight: 'bold'
+            },
+        }],
+        legend: {
+            show: false,
+            itemGap: 12,
+            data: ['通过', '未通过']
+        },
+        series: [{
+                name: 'Line 0',
+                type: 'pie',
+                clockWise: false,
+                hoverAnimation: false,
+                center: ['50%', '40%'],
+                radius: ['75%', '67.5%'],
+                itemStyle: {
+                    normal: {
+                        color: innerColor
+                    }
+                },
+                data: [{
+                    value: 50,
+                    name: '',
+                }],
+                label: {
+                    normal: {
+                        formatter: function(params) {
+                            var time = echartData[0].value;
+                            return '{time|' + time + '}{unit|min}';
+                        },
+                        position: 'center',
+                        textStyle: {
+                            fontSize: 38 * scale,
+                            fontWeight: 'bold',
+                            color: textColor
+                        },
+                        rich: rich
+                    }
+                }
+            },
+            {
+                name: 'Line 1',
+                type: 'pie',
+                clockWise: false,
+                hoverAnimation: false,
+                center: ['50%', '40%'],
+                radius: ['67.5%', '55%'],
+                color: color,
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: false
+                        },
+                        labelLine: {
+                            show: false
+                        },
+                    }
+                },
+                data: echartData,
+            }
+        ],
+    };
+    if (option && typeof option === "object") {
+        myChart6.setOption(option, true);
+        window.onresize = myChart6.resize;
     }
 }
