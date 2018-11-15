bcf2a0ee847b710bc63c89a2d7bd72e22801f49e
shiweiwei <shiweiwei@netposa.com>
2018-11-15 17:56:33 +0800

    add - json

--- js/asset-prevention.js
+++ js/asset-prevention.js
@@ -1,157 +1,137 @@
-$(function () {
-    drawChart2();
-    drawChart3();
-});
-function drawChart2() {
-    var dom2 = document.getElementById("chart2");
-    var myChart2 = echarts.init(dom2);
-    option = null;
-    option = {
-        legend: {
-            data: ['病毒库更新率'],
-            right: '10%',
-            textStyle: {
-                color: '#ccc'
-            }
-        },
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisLine: {
-                lineStyle: {
-                    color: '#1255F0'
-                }
-            },
-            axisLabel: {
-                show: true,
-                textStyle: {
-                    color: '#8ECEEE'
-                }
-            }
-        },
-        yAxis: {
-            type: 'value',
-            axisLine: {
-                lineStyle: {
-                    color: '#1255F0'
-                }
-            },
-            axisLabel: {
-                show: true,
-                textStyle: {
-                    color: '#8ECEEE'
-                }
-            },
-            splitLine: {
-                show: true,
-                lineStyle: {
-                    color: '#09206F'
-                }
-            }
-        },
-        series: [{
-            symbol: 'circle',
-            symbolSize: 6,
-            name: '病毒库更新率',
-            itemStyle: {
-                normal: {
-                    color: "#FF3838",
-                    lineStyle: {
-                        color: "#FF3838"
-                    }
-                }
-            },
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line'
-        }]
-    };
+var chartTypeState;
 
-    if (option && typeof option === "object") {
-        myChart2.setOption(option, true);
-        window.onresize = myChart2.resize;
-    }
+// 模态框获取的
+var postModalData;
+
+// 所有图表请求后暂存
+var storageData = {
+    drawLine: [],
+    drawVerticalBarH: []
 }
-// chart3
-function drawChart3() {
-    var dom3 = document.getElementById("chart3");
-    var myChart3 = echarts.init(dom3);
-    option = null;
-    option = {
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            }
-        },
-        legend: {
-            right: '10%',
-            textStyle: {
-                color: '#ccc'
-            },
-            data: ['直接访问', '邮件营销']
-        },
-        grid: {
-            x: 20,
-            y: 40,
-            x2: 20,
-            y2: 20,
-            containLabel: true
-        },
-        xAxis: [
-            {
-                type: 'category',
-                data: ['周一', '周二', '周三', '周四'],
-                axisLine: {
-                    lineStyle: {
-                        color: '#1255F0'
+
+var colorLineList = ['#FF3838'];
+var colorVerticalBarList = ['#45CE8D', '#9000FF'];
+
+$(function () {
+    layui.use('layer', function () {
+        var $ = layui.jquery, layer = layui.layer;
+        //触发事件
+        var active = {
+            setTop: function () {
+                var that = this;
+                layer.open({
+                    type: 2,
+                    title: '图表的模态框测试',
+                    area: ['70%', '70%'],
+                    shade: 0.3,
+                    offset: ['15%', '15%'],
+                    maxmin: true,
+                    anim: 1,
+                    content: '../pages/chartModal.html',
+                    yes: function () {
+                        $(that).click();
+                    },
+                    btn2: function () {
+                        layer.closeAll();
+                    },
+                    zIndex: layer.zIndex,
+                    success: function (layero) {
+                        // 子页面弹出成功回调
                     }
-                },
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#8ECEEE'
-                    }
-                }
+                });
             }
-        ],
-        yAxis: [{
-            type: 'value',
-            axisLine: {
-                lineStyle: {
-                    color: '#1255F0'
-                }
-            },
-            axisLabel: {
-                show: true,
-                textStyle: {
-                    color: '#8ECEEE'
-                }
-            },
-            splitLine: {
-                show: true,
-                lineStyle: {
-                    color: '#09206F'
-                }
+        };
+        $('.chart-click').on('click', function () {
+            chartTypeState = $(this).attr('data-chartType');
+            postModalData = storageData[chartTypeState];
+            var othis = $(this), method = othis.data('method');
+            active[method] ? active[method].call(this, othis) : '';
+        });
+    });
+
+
+    setTimeout(function () {
+        // chart2
+        $.ajax({
+            type: "GET",
+            data: "",
+            dataType: 'json',
+            url: "../test-json/line_1.json",
+            success: function (res) {
+                if (res.resultCode == 200) {
+                    var xAxisData = [];
+                    var sitemArr = [[]];
+                    var seriesData = []
+                    res.result.seriesData.forEach(function (item, idx) {
+                        xAxisData.push(item.date);
+                        sitemArr[0].push(item.viruses);
+                    });
+                    res.result.legendData.forEach(function (item, idx) {
+                        seriesData.push({
+                            name: item,
+                            type: 'line',
+                            symbol: 'circle',
+                            symbolSize: 8,
+                            lineStyle: {
+                                normal: {
+                                    width: 1
+                                }
+                            },
+                            itemStyle: {
+                                normal: {
+                                    color: colorLineList[idx],
+                                    borderWidth: 1
+                                }
+                            },
+                            data: sitemArr[idx]
+                        })
+                    });
+                    storageData.drawLine.push(res.result.legendData);
+                    storageData.drawLine.push(xAxisData);
+                    storageData.drawLine.push(seriesData);
+                    initChartFun.drawLine('chart2', storageData.drawLine);
+                };
             }
-        }],
-        series: [
-            {
-                name: '直接访问',
-                type: 'bar',
-                barWidth: 20,
-                data: [320, 332, 110, 230]
-            },
-            {
-                name: '邮件营销',
-                type: 'bar',
-                stack: '广告',
-                barWidth: 20,
-                data: [120, 132, 103, 240]
+        });
+
+        // chart3
+        $.ajax({
+            type: "GET",
+            data: "",
+            dataType: 'json',
+            url: "../test-json/verticalBar_2.json",
+            success: function (res) {
+                if (res.resultCode == 200) {
+                    var xAxisData = [];
+                    var sitemArr = [[], []];
+                    var seriesData = [];
+                    res.result.seriesData.forEach(function (item) {
+                        xAxisData.push(item[0]['date']);
+                        sitemArr[0].push(item[0]['openness']);
+                        sitemArr[1].push(item[0]['rate']);
+                    });
+                    res.result.legendData.forEach(function (item, idx) {
+                        seriesData.push({
+                            name: item,
+                            type: 'bar',
+                            itemStyle: {
+                                normal: {
+                                    color: colorVerticalBarList[idx]
+                                }
+                            },
+                            barWidth: 12,
+                            data: sitemArr[idx]
+                        })
+                    });
+                    storageData.drawVerticalBarH.push(res.result.legendData);
+                    storageData.drawVerticalBarH.push(xAxisData);
+                    storageData.drawVerticalBarH.push(seriesData);
+                    initChartFun.drawVerticalBarH('chart3', storageData.drawVerticalBarH);
+                };
             }
-        ]
-    };
-    if (option && typeof option === "object") {
-        myChart3.setOption(option, true);
-        window.onresize = myChart3.resize;
-    }
-}
+        });
+
+
+    }, 1000);
+
+});
