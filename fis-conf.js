a57cb14f19c583af8f5614083060f8e56df9af76
wdb <513052904@qq.com>
2019-01-25 14:59:24 +0800

    fis3配置

---
+++ fis-conf.js
@@ -0,0 +1,84 @@
+// default settings. fis3 release
+
+fis.match('::package', {
+  postpackager: fis.plugin('loader')
+});
+
+
+fis.match('*.{js,css,png,jpg,gif,jpeg,scss}', {
+  useHash: true
+});
+
+
+fis.match('/js/*.js', {
+  optimizer: fis.plugin('uglify-js'),
+  // packTo: '/js/aio.js',
+  parser: fis.plugin('translate-es6', {
+    presets: ['es2015']
+  })
+})
+
+
+fis.match('*.css', {
+  optimizer: fis.plugin('clean-css')
+})
+
+fis.match('*.scss', {
+  parser: fis.plugin('node-sass'),
+  optimizer: fis.plugin('clean-css'),
+  rExt: '.css'
+});
+
+
+fis.match('*.png', {
+  // 内置压缩png图片
+  optimizer: fis.plugin('png-compressor')
+});
+
+// 版本差异
+fis.media('debug').match('*.{js,css,png}', {
+  useHash: false,
+  useSprite: false,
+  optimizer: null
+})
+
+fis.media('prod').match('*.js', {
+  // 内置压缩js文件
+  optimizer: fis.plugin('uglify-js')
+});
+
+// Global end
+
+// default media is `dev`
+fis.media('dev')
+  .match('*', {
+    useHash: false,
+    optimizer: null
+  });
+
+// extends GLOBAL config
+fis.media('production');
+
+
+
+// fis3 release  发布
+// fis3 release -w  文件监听
+// fis3 release --wL  浏览器自动刷新
+// fis3 release -d /Users/my-name/work/htdocs  替代内置server 
+// fis3 release prod -d ./dist  发布版本
+
+
+
+// fis3 server start 启动
+// fis3 release -h
+
+
+
+// 文件合并
+// fis.match('*.{less,css}', {
+//   packTo: '/static/aio.css'
+// });
+
+// fis.match('*.js', {
+//   packTo: '/static/aio.js'
+// });
